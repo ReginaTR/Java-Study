f580d6335f5c94c85e2a2c9e1338ec573352cb92
ReginaTR <regininhar@hotmail.com>
2024-12-11 17:45:46 -0300

    teste

---
+++ src/br/com/study/JavaStudy.java
@@ -0,0 +1,12 @@
+package br.com.study;
+
+import br.com.study.model.Gato;
+
+public class JavaStudy {
+    public static void main(String[] args){
+        Gato gato = new Gato();
+        System.out.println(gato);
+        
+    }
+    
+}
